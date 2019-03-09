09b3a5b20d08b1c4dcc721d4cec8f9711bf0dffb
AsD Monio <asdmonio@gmail.com>
2019-03-09 21:35:00 +0100

    montana: Switch to permissive SELinux
    Change-Id: I4a31912e50a5efc79cd5945e3b04eef674578343

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
 # Kernel
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_LZ4C_DT := true
 TARGET_KERNEL_CONFIG := montana_defconfig
 
